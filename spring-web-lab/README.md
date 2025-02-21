5c94a139129912b317cb8d84d9097d31336c504f
mperor <mpietryga93@gmail.com>
2025-02-21 11:52:49 +0100

    Setup project for Spring Web Lab & verify actuator

---
+++ spring-web-lab/README.md
@@ -0,0 +1,5 @@
+# Spring Web Lab 🌐
+
+## Sources 🔗
+
+* [Java Senior Quiz 🧩](https://youtube.com/playlist?list=PLO5muTI694A5yRrEDNY1czqEUVbYHG_AH&si=KteWFowVcOSTQ8F-)
---
+++ spring-web-lab/src/main/java/pl/mperor/lab/spring/SpringWebLabApplication.java
@@ -0,0 +1,13 @@
+package pl.mperor.lab.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebLabApplication.class, args);
+    }
+
+}
