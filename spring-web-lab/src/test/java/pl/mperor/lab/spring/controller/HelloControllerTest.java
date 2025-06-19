f003f01077618b21182dfc9f78dd28ae5627bde4
mperor <mpietryga93@gmail.com>
2025-06-19 23:04:23 +0200

    Add Java Bean Validation with test case

--- spring-web-lab/src/test/java/pl/mperor/lab/spring/controller/HelloControllerTest.java
+++ spring-web-lab/src/test/java/pl/mperor/lab/spring/controller/HelloControllerTest.java
@@ -3,6 +3,7 @@ package pl.mperor.lab.spring.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
 @WebMvcTest
@@ -18,5 +19,28 @@ class HelloControllerTest {
                 .exchange()
                 .assertThat().hasStatusOk()
                 .bodyText().isEqualTo("🗺️ Hello World!");
+    }
+
+    @Test
+    void shouldReturnWelcomeWhenNameNotBlank() {
+        tester.post()
+                .uri("/hello/{name}", "NotBlank")
+                .exchange()
+                .assertThat().hasStatusOk()
+                .bodyText().isEqualTo("🤚 Welcome NotBlank!");
+    }
+
+    @Test
+    void shouldReturn400WhenNotValidContent() {
+        tester.post()
+                .uri("/hello/user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "name": "josh",
+                            "email": "not a valid email"
+                        }""")
+                .exchange()
+                .assertThat().hasStatus4xxClientError();
     }
 }
