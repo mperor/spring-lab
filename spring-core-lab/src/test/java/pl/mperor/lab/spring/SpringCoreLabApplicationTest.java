eb18372a24caff616c0bc48943843537bb2804b8
MP <mpietryga93@gmail.com>
2025-06-29 15:57:29 +0200

    Separated concerns: scope verification vs. bean ordering in Spring (#18)

--- spring-core-lab/src/test/java/pl/mperor/lab/spring/SpringCoreLabApplicationTest.java
+++ spring-core-lab/src/test/java/pl/mperor/lab/spring/SpringCoreLabApplicationTest.java
@@ -1,44 +1,12 @@
 package pl.mperor.lab.spring;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class SpringCoreLabApplicationTest {
 
-    @Autowired
-    private ApplicationContext context;
-    @Autowired
-    private Beans.BeanInstantiationTracker tracker;
-    @Autowired
-    private List<Beans.Orderable> orderables;
-
     @Test
     public void contextLoads() {
-    }
-
-    @Test
-    public void shouldInitializeSingletonAndPrototypeScopesCorrectly() {
-        assertThat(context.containsBean("singleton")).isTrue();
-        assertThat(context.containsBean("prototype")).isTrue();
-
-        assertThat(tracker.containsInitialized("singleton")).isTrue();
-        assertThat(tracker.containsInitialized("prototype")).isFalse();
-
-        assertThat(context.getBean(Beans.Singleton.class)).isSameAs(context.getBean(Beans.Singleton.class));
-        assertThat(context.getBean(Beans.Prototype.class)).isNotSameAs(context.getBean(Beans.Prototype.class));
-    }
-
-    @Test
-    public void shouldOrderBeansByPriority() {
-        assertThat(orderables)
-                .extracting(Beans.Orderable::getBeanName)
-                .containsExactly("minusPriorityOrder", "zeroPriorityOrder", "plusPriorityOrder");
     }
 }
