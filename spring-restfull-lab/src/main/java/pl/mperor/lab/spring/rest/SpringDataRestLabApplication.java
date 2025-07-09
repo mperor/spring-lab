f57fc70934d7491bedef8e23da814f734713d88c
mperor <mpietryga93@gmail.com>
2025-07-09 16:41:29 +0200

    Setup project for Spring Data Rest with HATEOS

---
+++ spring-restfull-lab/src/main/java/pl/mperor/lab/spring/rest/SpringDataRestLabApplication.java
@@ -0,0 +1,12 @@
+package pl.mperor.lab.spring.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRestLabApplication.class);
+    }
+}
---
+++ spring-restfull-lab/src/test/java/pl/mperor/lab/spring/rest/SpringDataRestLabApplicationTest.java
@@ -0,0 +1,13 @@
+package pl.mperor.lab.spring.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringDataRestLabApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
